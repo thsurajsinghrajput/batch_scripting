6684938b8fd5ecb8d7c54c5a57309923c8133667
thsurajsinghrajput <thsurajsinghrajput@gmail.com>
2023-11-09 20:45:54 +0530

    comments + varible+number done

---
+++ test.bat
@@ -0,0 +1,24 @@
+@REM ///// variable
+@echo off
+@REM set sms=HELLO
+@REM set num1=10
+@REM set num2 = 12
+
+@REM set num3=%num1%+%num2%
+@REM echo %num3% %sms%
+
+@REM ///// working with numbers 
+@REM SET /A a = 5
+@REM SET /A b = 10 
+@REM SET /A c = %a% + %b% 
+@REM echo %c%
+@REM  //// LOCAL AND GLOBAL variableset globalvar = 5
+@REM set /A globalvar = 5
+@REM SETLOCAL
+@REM set /A var = 13145
+@REM set /A var = %var% + 5
+@REM echo %var%
+@REM echo %globalvar%
+@REM ENDLOCAL
+ 
+
